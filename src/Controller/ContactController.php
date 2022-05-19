7d4a7aa01d60f57a79378c7b09cda785bd2bcd08
Jérôme <tyss@me.com>
2022-05-19 19:15:11 +0200

    test message

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,17 +5,18 @@ namespace App\Controller;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use Symfony\Component\Mime\Email;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Mime\RawMessage;
+
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact_index')]
-    public function index(Request $request, MailerInterface $mailer): Response
+    public function index(Request $request, MailerInterface $mailer, EntityManagerInterface $em): Response
     {
         //Mise en place du formulaire pour le rendu coté front
         $contact = new Contact();
@@ -27,7 +28,7 @@ class ContactController extends AbstractController
             //dd($form->getData());
             $message = (new Email())
                 ->from($contactFormData->getEmail())
-                ->to('toutenimage77@gmail.com')
+                ->to('tyss@wtproduction.com')
                 ->subject('vous avez reçu un email')
                 ->text(
                     'Envoyé par : ' . $contactFormData->getEmail() . \PHP_EOL .
@@ -36,8 +37,10 @@ class ContactController extends AbstractController
                     'text/plain'
                 );
             $mailer->send($message);
+            $em->persist($form->getData());
+            $em->flush();
             $this->addFlash('success', 'Vore message a été envoyé');
-            return $this->redirectToRoute('contact');
+            return $this->redirectToRoute('app_contact_index');
         }
         //Renvoi de la vue
         return $this->render('contact/index.html.twig', [
